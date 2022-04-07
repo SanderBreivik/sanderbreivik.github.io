56b005d4b6fa04873e34e20a1596998600e075be
SanderBreivik <s.b@live.no>
2022-04-07 20:41:47 +0200

    👌 IMPROVE: Add new toolbar component and SCSS

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Toolbar extends React.Component {
+  render() {
+    return (
+      <div className="toolbar-container">
+        <a href="./">Home</a>
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
